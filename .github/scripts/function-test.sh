112a68d69786b03a7e040dba734d927e1bfb9f0a
joachimhviid <joac1099@gmail.com>
2024-08-17 07:31:53 +0200

    Change backend url to check

--- .github/scripts/function-test.sh
+++ .github/scripts/function-test.sh
@@ -9,13 +9,10 @@ for i in {1..30}; do
 done
 # Check backend is ready
 for i in {1..30}; do
-  if curl -s http://localhost:9000; then
+  if curl -s http://localhost:9000/fortunes; then
     break
   fi
   sleep 1
 done
-
-curl -f http://localhost:8080 || exit 1
-curl -f http://localhost:9000 || exit 1
 
 docker compose down
